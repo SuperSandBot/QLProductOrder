17f3afa72e73392b50fe63c3ab683a2b1b5b57f8
SandBot <super.khang2@gmail.com>
2021-08-20 10:30:35 +0700

    Add project files.

---
+++ QLProductOrder/MainForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace QLProductOrder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbXemTatCaTrongThang = new System.Windows.Forms.CheckBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.STT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SoHD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgayDatHang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgayGiao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ThanhTien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbTong = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePickerEnd);
+            this.groupBox1.Controls.Add(this.dateTimePickerStart);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbXemTatCaTrongThang);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1354, 110);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông Tin Đơn Hàng";
+            // 
+            // dateTimePickerEnd
+            // 
+            this.dateTimePickerEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(424, 68);
+            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
+            this.dateTimePickerEnd.Size = new System.Drawing.Size(110, 22);
+            this.dateTimePickerEnd.TabIndex = 4;
+            this.dateTimePickerEnd.Value = new System.DateTime(2021, 8, 19, 20, 37, 10, 0);
+            this.dateTimePickerEnd.ValueChanged += new System.EventHandler(this.dateTimePickerEnd_ValueChanged);
+            // 
+            // dateTimePickerStart
+            // 
+            this.dateTimePickerStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerStart.Location = new System.Drawing.Point(275, 68);
+            this.dateTimePickerStart.Name = "dateTimePickerStart";
+            this.dateTimePickerStart.Size = new System.Drawing.Size(109, 22);
+            this.dateTimePickerStart.TabIndex = 3;
+            this.dateTimePickerStart.ValueChanged += new System.EventHandler(this.dateTimePickerStart_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(390, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "~";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thời Gian Giao Hàng";
+            // 
+            // cbXemTatCaTrongThang
+            // 
+            this.cbXemTatCaTrongThang.AutoSize = true;
+            this.cbXemTatCaTrongThang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbXemTatCaTrongThang.Location = new System.Drawing.Point(88, 33);
+            this.cbXemTatCaTrongThang.Name = "cbXemTatCaTrongThang";
+            this.cbXemTatCaTrongThang.Size = new System.Drawing.Size(219, 24);
+            this.cbXemTatCaTrongThang.TabIndex = 0;
+            this.cbXemTatCaTrongThang.Text = "Xem Tất Cả Trong Tháng";
+            this.cbXemTatCaTrongThang.UseVisualStyleBackColor = true;
+            this.cbXemTatCaTrongThang.CheckedChanged += new System.EventHandler(this.cbXemTatCaTrongThang_CheckedChanged);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.STT,
+            this.SoHD,
+            this.NgayDatHang,
+            this.NgayGiao,
+            this.ThanhTien});
+            this.dgv.Location = new System.Drawing.Point(12, 144);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(1354, 384);
+            this.dgv.TabIndex = 1;
+            // 
+            // STT
+            // 
+            this.STT.HeaderText = "STT";
+            this.STT.MinimumWidth = 6;
+            this.STT.Name = "STT";
+            this.STT.ReadOnly = true;
+            this.STT.Width = 50;
+            // 
+            // SoHD
+            // 
+            this.SoHD.HeaderText = "Số HĐ";
+            this.SoHD.MinimumWidth = 6;
+            this.SoHD.Name = "SoHD";
+            this.SoHD.ReadOnly = true;
+            this.SoHD.Width = 150;
+            // 
+            // NgayDatHang
+            // 
+            this.NgayDatHang.HeaderText = "Ngày Đặt Hàng";
+            this.NgayDatHang.MinimumWidth = 6;
+            this.NgayDatHang.Name = "NgayDatHang";
+            this.NgayDatHang.ReadOnly = true;
+            this.NgayDatHang.Width = 200;
+            // 
+            // NgayGiao
+            // 
+            this.NgayGiao.HeaderText = "Ngày Giao";
+            this.NgayGiao.MinimumWidth = 6;
+            this.NgayGiao.Name = "NgayGiao";
+            this.NgayGiao.ReadOnly = true;
+            this.NgayGiao.Width = 200;
+            // 
+            // ThanhTien
+            // 
+            this.ThanhTien.HeaderText = "Thành Tiền";
+            this.ThanhTien.MinimumWidth = 6;
+            this.ThanhTien.Name = "ThanhTien";
+            this.ThanhTien.ReadOnly = true;
+            this.ThanhTien.Width = 150;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(981, 541);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tổng Cộng:";
+            // 
+            // tbTong
+            // 
+            this.tbTong.Enabled = false;
+            this.tbTong.Location = new System.Drawing.Point(1069, 538);
+            this.tbTong.Name = "tbTong";
+            this.tbTong.Size = new System.Drawing.Size(297, 22);
+            this.tbTong.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1378, 567);
+            this.Controls.Add(this.tbTong);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Thông Tin Đơn Hàng";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbXemTatCaTrongThang;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbTong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn STT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoHD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NgayDatHang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NgayGiao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ThanhTien;
+    }
+}
+
---
+++ QLProductOrder/MainForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLProductOrder.Model;
+
+namespace QLProductOrder
+{
+    public partial class MainForm : Form
+    {
+        dbProductOrder data = new dbProductOrder();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Updatedgv();
+            dateTimePickerStart.Value = data.Invoices.Select(p => p.OrderDate).First();
+            dateTimePickerEnd.Value = data.Invoices.Select(p => p.DeliveryDate).First();
+        }
+
+        private void cbXemTatCaTrongThang_CheckedChanged(object sender, EventArgs e)
+        {
+            Updatedgv();
+        }
+
+        private void Updatedgv()
+        {
+            int i = 0;
+            int tong = 0;
+            dgv.Rows.Clear();
+
+            var q = data.Orders.GroupBy(p => p.Invoice)
+            .Select(g => new {
+                Invoice = g.Key,
+                OrderDate = g.Key.OrderDate,
+                DeliveryDate = g.Key.DeliveryDate,
+                Sum = g.Sum(p => p.Quantity * p.Price),
+            }).ToList();
+
+            if(cbXemTatCaTrongThang.Checked)
+            {
+                foreach (var order in q)
+                {
+                    if (order.OrderDate.Month == dateTimePickerStart.Value.Month)
+                    {
+                        dgv.Rows.Add();
+                        dgv.Rows[i].Cells[0].Value = i + 1;
+                        dgv.Rows[i].Cells[1].Value = order.Invoice.InvoiceNo;
+                        dgv.Rows[i].Cells[2].Value = order.OrderDate.Date;
+                        dgv.Rows[i].Cells[3].Value = order.DeliveryDate.Date;
+                        dgv.Rows[i].Cells[4].Value = order.Sum;
+
+                        tong += (int)order.Sum;
+                        i++;
+                    }
+                }
+                tbTong.Text = tong.ToString();
+            }
+            else
+            {
+                foreach (var order in q)
+                {
+                    if (order.OrderDate >= dateTimePickerStart.Value && order.DeliveryDate <= dateTimePickerEnd.Value)
+                    {
+                        dgv.Rows.Add();
+                        dgv.Rows[i].Cells[0].Value = i + 1;
+                        dgv.Rows[i].Cells[1].Value = order.Invoice.InvoiceNo;
+                        dgv.Rows[i].Cells[2].Value = order.OrderDate.Date;
+                        dgv.Rows[i].Cells[3].Value = order.DeliveryDate.Date;
+                        dgv.Rows[i].Cells[4].Value = order.Sum;
+
+                        tong += (int)order.Sum;
+                        i++;
+                    }
+                }
+                tbTong.Text = tong.ToString();
+            }
+
+        }
+
+        private void dateTimePickerStart_ValueChanged(object sender, EventArgs e)
+        {
+            Updatedgv();
+        }
+
+        private void dateTimePickerEnd_ValueChanged(object sender, EventArgs e)
+        {
+            Updatedgv();
+        }
+    }
+}
